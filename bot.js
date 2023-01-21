f6339db025d538cf153096ecd19a5462496b626e
Mariana Borges Machado de Andrade <118206264+mariborges22@users.noreply.github.com>
2023-01-21 11:08:18 -0300

    Update bot.js
    Commands that put the bot on and makes it execute a commands like pingpong, but in this case i wanted to difference a bit, so i put a commands named 'toradora', when user type '/tora' and bot replies with 'Dora!'

--- bot.js
+++ bot.js
@@ -1,1 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'tora') {
+    await interaction.reply('Dora!');
+  }
+});
+
+
+  
+
+
+
+client.login(token);
